4c4453991d82992d0c69056dd5327774f48480fd
aimpugn <aimpugn@gmail.com>
2024-09-10 23:34:38 +0900

    appspec.yaml -> appspec.yml 및 start.sh 수정
    - 기본값이 yaml이 아닌 yml이어서 파일명을 수정합니다.
    - scripts/start.sh: 절대 경로 사용하도록 수정
    
    Reference:
    - https://docs.aws.amazon.com/ko_kr/codedeploy/latest/userguide/deployments-local.html
    - https://docs.aws.amazon.com/codedeploy/latest/userguide/reference-appspec-file-structure.html

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
 
-UBUNTU_USER_HOME="/home/ubuntu"
-DEPLOYMENT_LOG="$UBUNTU_USER_HOME/codedeploy.$(date +'%Y%m%d_%H%M%S').log"
-VAR_WWW_APP_DIR="/var/www/aimpugn.me/"
-VAR_WWW_APP_PATH="$VAR_WWW_APP_DIR/backend.jar"
+whoami
 
-(cd ./backend && ./gradlew bootJar) > "$DEPLOYMENT_LOG"
-cp ./backend/build/libs/backend-*.jar "$VAR_WWW_APP_PATH"
+DEPLOYMENT_LOG="/var/log/codedeploy.$(date +'%Y%m%d').log"
+# 타겟 파일 경로
+VAR_WWW_APP_DIR="/var/www/aimpugn.me"
+TARGET_JAR="$VAR_WWW_APP_DIR/backend.jar"
 
-java -jar "$VAR_WWW_APP_PATH"
+echo "Starting deployment at $(date)" | sudo tee -a "$DEPLOYMENT_LOG"
+
+(cd "$VAR_WWW_APP_DIR/backend" && sudo ./gradlew bootJar) | tee "$DEPLOYMENT_LOG"
+
+sudo chown -R ubuntu:ubuntu "$VAR_WWW_APP_DIR/backend"
+
+# 실제 파일 경로를 확인합니다.
+SOURCE_JAR="$(realpath "$VAR_WWW_APP_DIR"/backend/build/libs/backend-*.jar)"
+
+# 파일이 존재하는지 확인합니다.
+if [ ! -f "$SOURCE_JAR" ]; then
+    echo "Error: $SOURCE_JAR JAR file not found."
+    exit 1
+fi
+
+echo "Move $SOURCE_JAR to $TARGET_JAR"
+
+mv "$SOURCE_JAR" "$TARGET_JAR"
+
+java -jar "$TARGET_JAR"
