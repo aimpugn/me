50aa6a04133b2046b98a9830eb52424a665d6114
aimpugn <aimpugn@gmail.com>
2024-09-10 17:52:33 +0900

    CodeDeploy로 배포하기 위한 HelloWorld 테스트 애플리케이션

---
+++ backend/src/main/java/me/aimpugn/backend/Controller/HomeController.java
@@ -0,0 +1,14 @@
+package me.aimpugn.backend.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    String home() {
+
+        return "Hello Spring";
+    }
+}
